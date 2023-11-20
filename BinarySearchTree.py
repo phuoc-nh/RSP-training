f7ab6b874d5e2e716164fa00ce148c672c558afb
TheCoffeeGuy <huuphuoc252513@gmail.com>
2023-11-20 22:51:11 +1100

    Finish add node BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -6,11 +6,29 @@ class Node:
     
 def insert(root, value):
     
+    if not root:
+        return Node(value)
+    
+    if root.value == value:
+        return root
+    elif root.value < value:
+        root.right = insert(root.right, value)
+    
+    else:
+        root.left = insert(root.left,value)
+    return root
+    
 
 def inorder(root):
     if root:
         inorder(root.left)
-        print(root.value, end=" ")
+        print(root.value)
         inorder(root.right)
     
+
+r = Node(50)
+r =insert(r, 30)
+r = insert(r, 20)
+
+inorder(r)
         
