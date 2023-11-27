e777ad0d6e0c3502ff81554701faeb976515550e
TheCoffeeGuy <huuphuoc252513@gmail.com>
2023-11-27 21:01:26 +1100

    Code topo sort

---
+++ TopoSort.py
@@ -0,0 +1,43 @@
+# def recursiveDfs(u):
+#     visited[u] = True
+#     for v in graph[u]:
+#         if not visited[v]:
+#             recursiveDfs[v]
+    
+#     resTopo.append(u)
+
+# def topoSort():
+#     visited = [False] * V
+#     for i in range(V):
+#         if not visited[i]:
+#             recursiveDFS(i)
+            
+#     resTopo.reverse()
+    
+def main():
+    
+    V, E = map(int, input().split())
+    graph = [[] for i in range(V)]
+    for i in range(E):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+    # print(graph)
+    # print(v)
+    visited = [False] * V
+    topoSort = []
+    # print(visited)
+    def recursiveDfs(u):
+        visited[u] = True
+        for v in graph[u]:
+            print('vvv',v)
+            if not visited[v]:
+                recursiveDfs(v)
+        topoSort.append(u)
+    
+    for i in range(V):
+        if not visited[i]:
+            recursiveDfs(i)
+            
+    topoSort.reverse()
+    print('topo sort', topoSort)
+main()
