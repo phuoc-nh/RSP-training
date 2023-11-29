c80ee72b0eac82799e87087e2232a0e7099eb546
TheCoffeeGuy <huuphuoc252513@gmail.com>
2023-11-29 19:26:03 +1100

    Binary tree

---
+++ BinaryTree.py
@@ -0,0 +1,77 @@
+from queue import Queue
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+def constructBinaryTree(arr, i, n):
+    root = None
+    if i < n:
+        root = Node(arr[i])
+        root.left = constructBinaryTree(arr, i * 2 + 1, n)
+        root.right = constructBinaryTree(arr, i * 2 + 2, n)
+        
+    return root
+
+def inorderTraverse(root):
+    if root:
+        traverse(root.left)
+        print(root.value, end=" ")
+        traverse(root.right)
+
+def inorderTraverse(root):
+    if root:
+        traverse(root.left)
+        print(root.value, end=" ")
+        traverse(root.right)
+    
+def buildTreeIter(arr):
+    root = Node(arr[0])
+
+    q = Queue()
+    q.put(root)
+    i = 1
+    
+    while i < len(arr):
+        node = q.get()
+        node.left = Node(arr[i])
+        i += 1
+        q.put(node.left)
+
+        node.right = Node(arr[i])
+        i += 1
+        q.put(node.right)
+    
+    return root
+
+def insertNode(root, value):
+    q = Queue()
+    q.put(root)
+    
+    while not q.empty():
+        cur = q.get()
+        
+        if not cur.left:
+            cur.left = Node(value)
+            break
+        else:
+            q.put(cur.left)
+
+        if not cur.right:
+            cur.right = Node(value)
+            break
+        else:
+            q.put(cur.right)
+
+            
+        
+    
+
+arr = [1, 2, 3, 4, 5, 6, 6, 6, 6]
+# root = constructBinaryTree(arr, 0, len(arr))
+# traverse(root)
+
+root = buildTreeIter(arr)
+traverse(root)
