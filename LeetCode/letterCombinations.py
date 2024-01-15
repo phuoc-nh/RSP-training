b3bdabdafb8338af3f03386884ee244dbaf618f0
TheCoffeeGuy <huuphuoc252513@gmail.com>
2024-01-15 21:38:18 +1100

    leetcode updaet

--- LeetCode/letterCombinations.py
+++ LeetCode/letterCombinations.py
@@ -1,2 +1,29 @@
 def letterCombinations(digits):
+    m = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
+    res = []
     
+    def backtrack(word, remainDigits):
+        if word and len(word) == len(digits):
+            res.append(word)
+            return
+        
+        for i in range(len(remainDigits)):
+            for d in m[remainDigits[i]]:
+                backtrack(word+d, remainDigits[i+1:])
+        
+    
+    backtrack('', digits)
+    print(res)
+    return res
+
+letterCombinations("23")
+        
