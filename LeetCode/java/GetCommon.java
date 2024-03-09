7b9e7462527f3cf9420e0b3ef278b954f75ded21
Bee <huuphuoc252513@gmail.com>
2024-03-09 19:45:58 +1100

    leetcode update

---
+++ LeetCode/java/GetCommon.java
@@ -0,0 +1,27 @@
+public class GetCommon {
+    public static void main(String[] args) {
+        int[] nums1 = {
+            1,2,3
+        };
+        int[] nums2 = {
+                2,4
+        };
+        System.out.println(getCommon(nums1, nums2));
+    }
+
+    static int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+}
