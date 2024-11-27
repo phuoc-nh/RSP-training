3b9b578aecccdf97109e2248eb111ac1baaf6312
Phuoc Nguyen <huuphuoc252513@gmail.com>
2024-11-27 22:10:01 +0700

    neetcode

--- LeetCode/Neetcode/java/Main.java
+++ LeetCode/Neetcode/java/Main.java
@@ -24,9 +24,24 @@ public class Main {
 //
 //		var dec = decode(code);
 
-		var nums = new int[] {-1,0,1,2,3};
-		var res = productExceptSelf(nums);
+//		var nums = new int[] {-1,0,1,2,3};
+//		var res = productExceptSelf(nums);
 
+
+		char[][] sudoku = new char[][]{
+				{'5','3','.','.','7','.','.','.','.'}
+				,{'6','.','.','1','9','5','.','.','.'}
+				,{'.','9','8','.','.','.','.','6','.'}
+				,{'8','.','.','.','6','.','.','.','3'}
+				,{'4','.','.','8','.','3','.','.','1'}
+				,{'7','.','.','.','2','.','.','.','6'}
+				,{'.','6','.','.','.','.','2','8','.'}
+				,{'.','.','.','4','1','9','.','.','5'}
+				,{'.','.','.','.','8','.','.','7','9'}
+		};
+
+		var res = isValidSudoku(sudoku);
+		System.out.println(">>> res " + res);
 
 
 	}
@@ -229,6 +244,56 @@ public class Main {
 
 		return res;
 
+	}
+
+	static public boolean isValidSudoku(char[][] board) {
+//		check row
+		for (int i = 0; i < 9; i++) {
+			HashSet set = new HashSet();
+			for (int j = 0; j < 9; j++) {
+				System.out.println(">> row " + board[i][j]);
+				if (board[i][j] == '.') {
+					continue;
+				}
+				if (set.contains(board[i][j])) {
+					return false;
+				}
+				set.add(board[i][j]);
+			}
+		}
+
+		for (int i = 0; i < 9; i++) {
+			HashSet set = new HashSet();
+			for (int j = 0; j < 9; j++) {
+				System.out.println("col " + board[j][i]);
+				if (board[j][i] == '.') {
+					continue;
+				}
+				if (set.contains(board[j][i])) {
+					return false;
+				}
+				set.add(board[j][i]);
+			}
+		}
+
+		for (int i = 0; i < 9; i = i + 3) {
+			for (int j = 0; j < 9; j = j + 3) {
+				HashSet set = new HashSet();
+				for (int k = i; k < i + 3; k++) {
+					for (int l = j; l < j + 3; l++) {
+						if (board[k][l] == '.') {
+							continue;
+						}
+						if (set.contains(board[k][l])) {
+							return false;
+						}
+						set.add(board[k][l]);
+					}
+				}
+			}
+		}
+
+		return true;
 	}
 
 }
