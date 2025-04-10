a1934ec8adeb5dd3cb1daa5286c78071a6fac6cf
Phuoc Nguyen <huuphuoc252513@gmail.com>
2025-04-10 10:20:13 +1000

    leetcode

--- LeetCode/python/coinChange.py
+++ LeetCode/python/coinChange.py
@@ -1,18 +1,42 @@
-import math
 
+# def coinChange(coins, amount):
+    
+#     cache = {}
+#     def dfs(amount):
+#         if amount == 0:
+#             return 0
+        
+#         if amount in cache:
+#             return cache[amount]
+        
+#         res = 1e9
+        
+#         for num in coins:
+#             if amount - num >= 0:
+#                 res = min(res, 1 + dfs(amount - num))
+                
+#         cache[amount] = res
+                
+#         return res
+    
+#     res = dfs(amount)
+#     return res if res != 1e9 else -1
+        
+        
 def coinChange(coins, amount):
-    dp = [math.inf for i in range(amount+1)]
+    dp = [1e9] * (amount + 1)
     dp[0] = 0
     
-    for i in range(1, amount+1):
-        for c in coins:
-            if i - c >= 0:
-                dp[i] = min(dp[i], 1 + dp[i - c])
+    for curAmount in range(1, amount + 1):
+        for coin in coins:
+            if curAmount - coin >= 0:
+                dp[curAmount] = min(dp[curAmount], 1 + dp[curAmount - coin])
                 
-    # print(dp)
-
-    return dp[-1] if dp[-1] != math.inf else -1
-
-coins = [2]
-amount = 3
+    return dp[amount] if dp[amount] != 1e9 else -1
+            
+        
+            
+    
+coins = [1,3,4,5]
+amount = 7
 print(coinChange(coins, amount))
